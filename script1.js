86caef44974289cf9757a4bc563d712702a3305b
Megan Huang <megan@10-21-49-41.dynapool.wireless.nyu.edu>
2022-11-09 20:42:18 -0500

    changed script1 + script2 from functions to conditional statements

--- script1.js
+++ script1.js
@@ -1,14 +1,8 @@
+var string1 = "hi"
+var string2 = "hi"
 
-function check(string1, string2){
-
-  if (string1 == string2){
+if (string1 == string2){
     alert(string1 + " is equal to " + string2);
-  } else{
+} else {
     alert(string1 + " is not equal to " + string2);
-  }
-
 }
-
-var checkEqual = check("cat", "cat");
-
-console.log(checkEqual);
